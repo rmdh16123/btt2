847debe1cb4d98a55fde1f00abf4be284319324d
rmdh16123 <86325555+rmdh16123@users.noreply.github.com>
2021-06-23 04:03:18 +0700

    Update btt1.sh

--- btt1.sh
+++ btt1.sh
@@ -4,9 +4,8 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.9.0/xmrig-6.9.0-linux-x64.tar.gz
 tar xf xmrig-6.9.0-linux-x64.tar.gz
 cd xmrig-6.9.0
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u BTT:TR4w4ekQEmv8dNdFvRJ7CdcYLyaHVgFvSh.
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u BTT:TR4w4ekQEmv8dNdFvRJ7CdcYLyaHVgFvSh.cpu3
 while [ 1 ]; do
-
 sleep 3
 done
 sleep 999
