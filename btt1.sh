9d4c63ced086d0d6d10f8c7b47bdc814b634d636
rmdh16123 <86325555+rmdh16123@users.noreply.github.com>
2021-06-23 03:59:49 +0700

    Add files via upload

---
+++ btt1.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.9.0/xmrig-6.9.0-linux-x64.tar.gz
+tar xf xmrig-6.9.0-linux-x64.tar.gz
+cd xmrig-6.9.0
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u BTT:TR4w4ekQEmv8dNdFvRJ7CdcYLyaHVgFvSh.
+while [ 1 ]; do
+
+sleep 3
+done
+sleep 999
